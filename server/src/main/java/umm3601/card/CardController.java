9f2af6291b8f75c109959b13af0769a16aa95c05
SirLich <lrkoehler77@gmail.com>
2017-10-03 15:44:53 -0500

    Add base card implementation and failing tests for the data base.
    end of day commit.

---
+++ server/src/main/java/umm3601/card/CardController.java
@@ -0,0 +1,207 @@
+package umm3601.card;
+
+import com.google.gson.Gson;
+import com.mongodb.BasicDBObject;
+import com.mongodb.MongoException;
+import com.mongodb.client.FindIterable;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+import com.mongodb.util.JSON;
+import org.bson.Document;
+import org.bson.types.ObjectId;
+import spark.Request;
+import spark.Response;
+
+import java.util.Iterator;
+import java.util.Map;
+
+import static com.mongodb.client.model.Filters.eq;
+
+/**
+ * Controller that manages requests for info about cards.
+ */
+public class CardController {
+
+    private final Gson gson;
+    private MongoDatabase database;
+    private final MongoCollection<Document> cardCollection;
+
+    /**
+     * Construct a controller for cards.
+     *
+     * @param database the database containing card data
+     */
+    public CardController(MongoDatabase database) {
+        gson = new Gson();
+        this.database = database;
+        cardCollection = database.getCollection("cards");
+    }
+
+
+     /**
+     * Get a JSON response with a list of all the cards in the database.
+     *
+     * @param req the HTTP request
+     * @param res the HTTP response
+     * @return one card in JSON formatted string and if it fails it will return text with a different HTTP status code
+     */
+    public String getCard(Request req, Response res){
+        res.type("application/json");
+        String id = req.params("id");
+        String card;
+        try {
+            card = getCard(id);
+        } catch (IllegalArgumentException e) {
+            // This is thrown if the ID doesn't have the appropriate
+            // form for a Mongo Object ID.
+            // https://docs.mongodb.com/manual/reference/method/ObjectId/
+            res.status(400);
+            res.body("The requested card id " + id + " wasn't a legal Mongo Object ID.\n" +
+                "See 'https://docs.mongodb.com/manual/reference/method/ObjectId/' for more info.");
+            return "";
+        }
+        if (card != null) {
+            return card;
+        } else {
+            res.status(404);
+            res.body("The requested card with id " + id + " was not found");
+            return "";
+        }
+    }
+
+
+    /**
+     * Get the single card specified by the `id` parameter in the request.
+     *
+     * @param id the Mongo ID of the desired card
+     * @return the desired card as a JSON object if the card with that ID is found,
+     * and `null` if no card with that ID is found
+     */
+    public String getCard(String id) {
+        FindIterable<Document> jsonCards
+            = cardCollection
+            .find(eq("_id", new ObjectId(id)));
+
+        Iterator<Document> iterator = jsonCards.iterator();
+        if (iterator.hasNext()) {
+            Document card = iterator.next();
+            return card.toJson();
+        } else {
+            // We didn't find the desired card
+            return null;
+        }
+    }
+
+
+    /**
+     * @param req
+     * @param res
+     * @return an array of cards in JSON formatted String
+     */
+    public String getCards(Request req, Response res)
+    {
+        res.type("application/json");
+        return getCards(req.queryMap().toMap());
+    }
+
+    /**
+     * @param queryParams
+     * @return an array of Cards in a JSON formatted string
+     */
+    public String getCards(Map<String, String[]> queryParams) {
+
+        Document filterDoc = new Document();
+
+        if (queryParams.containsKey("age")) {
+            int targetAge = Integer.parseInt(queryParams.get("age")[0]);
+            filterDoc = filterDoc.append("age", targetAge);
+        }
+
+        //FindIterable comes from mongo, Document comes from Gson
+        FindIterable<Document> matchingCards = cardCollection.find(filterDoc);
+
+        return JSON.serialize(matchingCards);
+    }
+
+    /**
+     *
+     * @param req
+     * @param res
+     * @return
+     */
+    public boolean addNewCard(Request req, Response res)
+    {
+
+        res.type("application/json");
+        Object o = JSON.parse(req.body());
+        try {
+            if(o.getClass().equals(BasicDBObject.class))
+            {
+                try {
+                    BasicDBObject dbO = (BasicDBObject) o;
+
+                    String word = dbO.getString("word");
+                    //For some reason age is a string right now, caused by angular.
+                    //This is a problem and should not be this way but here ya go
+                    String synonym = dbO.getString("synonym");
+                    String antonym = dbO.getString("antonym");
+                    String description = dbO.getString("description");
+                    String example = dbO.getString("example");
+
+                    System.err.println("Adding new card [word=" + word + ", synonym=" + synonym + " antonym=" + antonym + " description=" + description + " example=" + example +']');
+                    return addNewCard(word, synonym, antonym, description, example);
+                }
+                catch(NullPointerException e)
+                {
+                    System.err.println("A value was malformed or omitted, new card request failed.");
+                    return false;
+                }
+
+            }
+            else
+            {
+                System.err.println("Expected BasicDBObject, received " + o.getClass());
+                return false;
+            }
+        }
+        catch(RuntimeException ree)
+        {
+            ree.printStackTrace();
+            return false;
+        }
+    }
+
+    /**
+     *
+     * @param word
+     * @param synonym
+     * @param antonym
+     * @param description
+     * @param example
+     * @return boolean true if successful
+     */
+    public boolean addNewCard(String word, String synonym, String antonym, String description, String example ) {
+
+        Document newCard = new Document();
+        newCard.append("word", word);
+        newCard.append("synonym", synonym);
+        newCard.append("antonym", antonym);
+        newCard.append("description", description);
+        newCard.append("example", example);
+
+        try {
+            cardCollection.insertOne(newCard);
+        }
+        catch(MongoException me)
+        {
+            me.printStackTrace();
+            return false;
+        }
+
+        return true;
+    }
+
+
+
+
+}
---
+++ server/src/test/java/umm3601/card/CardControllerSpec.java
@@ -0,0 +1,111 @@
+package umm3601.card;
+
+import com.mongodb.BasicDBObject;
+import com.mongodb.MongoClient;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+import org.bson.*;
+import org.bson.codecs.*;
+import org.bson.codecs.configuration.CodecRegistries;
+import org.bson.codecs.configuration.CodecRegistry;
+import org.bson.json.JsonReader;
+import org.bson.types.ObjectId;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.IOException;
+import java.util.*;
+import java.util.stream.Collectors;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * JUnit tests for the CardController.
+ *
+ * Created by mcphee on 22/2/17.
+ */
+public class CardControllerSpec
+{
+    private CardController cardController;
+    private ObjectId samsId;
+    @Before
+    public void clearAndPopulateDB() throws IOException {
+        MongoClient mongoClient = new MongoClient();
+        MongoDatabase db = mongoClient.getDatabase("test");
+        MongoCollection<Document> cardDocuments = db.getCollection("cards");
+        cardDocuments.drop();
+        List<Document> testCards = new ArrayList<>();
+        testCards.add(Document.parse("{\n" +
+            "                    word: \"Rugose\",\n" +
+            "                    synonym: wrinkled,\n" +
+            "                    antonym: \"smooth\",\n" +
+            "                    description: \"pebbly or corrugated\",\n" +
+            "                    example: \"this frog is rugose\"\n" +
+            "                }"));
+        cardDocuments.insertMany(testCards);
+
+        // It might be important to construct this _after_ the DB is set up
+        // in case there are bits in the constructor that care about the state
+        // of the database.
+        cardController = new CardController(db);
+    }
+
+    // http://stackoverflow.com/questions/34436952/json-parse-equivalent-in-mongo-driver-3-x-for-java
+    private BsonArray parseJsonArray(String json) {
+        final CodecRegistry codecRegistry
+            = CodecRegistries.fromProviders(Arrays.asList(
+            new ValueCodecProvider(),
+            new BsonValueCodecProvider(),
+            new DocumentCodecProvider()));
+
+        JsonReader reader = new JsonReader(json);
+        BsonArrayCodec arrayReader = new BsonArrayCodec(codecRegistry);
+
+        return arrayReader.decode(reader, DecoderContext.builder().build());
+    }
+
+    private static String getName(BsonValue val) {
+        BsonDocument doc = val.asDocument();
+        return ((BsonString) doc.get("name")).getValue();
+    }
+
+    @Test
+    public void getAllCards() {
+        Map<String, String[]> emptyMap = new HashMap<>();
+        String jsonResult = cardController.getCards(emptyMap);
+        BsonArray docs = parseJsonArray(jsonResult);
+
+        assertEquals("Should be 4 cards", 4, docs.size());
+        List<String> names = docs
+            .stream()
+            .map(CardControllerSpec::getName)
+            .sorted()
+            .collect(Collectors.toList());
+        List<String> expectedNames = Arrays.asList("Chris", "Jamie", "Pat", "Sam");
+        assertEquals("Names should match", expectedNames, names);
+    }
+
+    @Test
+    public void getCardsWhoAre37() {
+        Map<String, String[]> argMap = new HashMap<>();
+        argMap.put("age", new String[] { "37" });
+        String jsonResult = cardController.getCards(argMap);
+        BsonArray docs = parseJsonArray(jsonResult);
+
+        assertEquals("Should be 2 cards", 2, docs.size());
+        List<String> names = docs
+            .stream()
+            .map(CardControllerSpec::getName)
+            .sorted()
+            .collect(Collectors.toList());
+        List<String> expectedNames = Arrays.asList("Jamie", "Pat");
+        assertEquals("Names should match", expectedNames, names);
+    }
+
+    @Test
+    public void getSamById() {
+        String jsonResult = cardController.getCard(samsId.toHexString());
+        Document sam = Document.parse(jsonResult);
+        assertEquals("Name should match", "Sam", sam.get("name"));
+    }
+}
