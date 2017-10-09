fa119f8a28c583f74a6b0bb1e4f16969eee7e046
unwarysage <shawx538@morris.umn.edu>
2017-10-08 23:39:36 -0500

    add card service
    connecting the card creation form on the front end to cardcontrlloer in the backend.
    needs front end and eventually e2e tests.
    Closes #2

--- client/src/app/card/card-create/card-create.component.ts
+++ client/src/app/card/card-create/card-create.component.ts
@@ -1,15 +1,33 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit, EventEmitter, Output} from '@angular/core';
+import {CardService} from "../card.service"
 
 @Component({
-  selector: 'app-card-create',
-  templateUrl: './card-create.component.html',
-  styleUrls: ['./card-create.component.css']
+    selector: 'app-card-create',
+    templateUrl: './card-create.component.html',
+    styleUrls: ['./card-create.component.css'],
+    providers: []
+
 })
 export class CardCreateComponent implements OnInit {
+    @Output() submitCard: EventEmitter<any> = new EventEmitter();
 
-  constructor() { }
 
-  ngOnInit() {
-  }
+    public word: string = null;
+    public synonym: string = null;
+    public antonym: string = null;
+    public general_sense: string = null;
+    public example_usage: string = null;
+
+    constructor(public cardService: CardService) {
+    }
+
+    submitNewCard() {
+        this.cardService.addNewCard(this.word,this.synonym,this.antonym,this.general_sense,this.example_usage).subscribe(succeeded => {
+            console.log("added card succesfully: " + succeeded);
+        })
+    }
+
+    ngOnInit() {
+    }
 
 }
