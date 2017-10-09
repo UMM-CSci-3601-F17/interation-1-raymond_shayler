020696fe9b259e20221bc22f64fecf75b0cca507
unwarysage <shawx538@morris.umn.edu>
2017-10-08 21:55:12 -0500

    add basic frontend for card creation
    needs angular setup for making the buttons do things

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -10,10 +10,12 @@ import {HomeComponent} from './home/home.component';
 import {UserComponent} from "./users/user.component";
 import {UserListComponent} from './users/user-list.component';
 import {UserListService} from './users/user-list.service';
+import {CardCreateComponent} from "./card/card-create/card-create.component";
 import {Routing} from './app.routes';
 import {APP_BASE_HREF} from "@angular/common";
 
 import {SharedModule} from "./shared.module";
+import {CardComponent} from "./card/card/card.component";
 
 
 @NgModule({
@@ -28,7 +30,9 @@ import {SharedModule} from "./shared.module";
         AppComponent,
         HomeComponent,
         UserListComponent,
-        UserComponent
+        UserComponent,
+        CardCreateComponent,
+        CardComponent,
     ],
     providers: [
         UserListService,
--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -3,11 +3,13 @@ import {ModuleWithProviders} from '@angular/core';
 import {Routes, RouterModule} from '@angular/router';
 import {HomeComponent} from './home/home.component';
 import {UserListComponent} from "./users/user-list.component";
+import {CardCreateComponent} from "./card/card-create/card-create.component";
 
 // Route Configuration
 export const routes: Routes = [
     {path: '', component: HomeComponent},
-    {path: 'users', component: UserListComponent}
+    {path: 'users', component: UserListComponent},
+    {path: 'cardcreate', component: CardCreateComponent}
 ];
 
 export const Routing: ModuleWithProviders = RouterModule.forRoot(routes);
