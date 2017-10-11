f43f7142f35f08f8b6a73b952e2aec103e167f85
rabex028 <rabex028@morris.umn.edu>
2017-10-10 20:35:24 -0500

    Display random card hints
    Our card display now contains a hint button which will display a random hint below the word. The card by default starts by displaying no hints, and will incrementally display its hints as the hint button is pressed.

--- client/src/app/card/card/card.component.ts
+++ client/src/app/card/card/card.component.ts
@@ -16,6 +16,14 @@ export class CardComponent implements OnInit {
     public general_sense: string = null;
     public example_usage: string = null;
 
+    public hiddenSynonym: string = null;
+    public hiddenAntonym: string = null;
+    public hiddenGeneral_sense: string = null;
+    public hiddenExample_usage: string = null;
+
+    public hintArray: any[4];
+    public hintCounter: number = 0;
+
     constructor(public cardService: CardService){
 
     }
@@ -24,12 +32,63 @@ export class CardComponent implements OnInit {
         console.log("Button click?");
         this.cardService.getRandomCard().subscribe(succeeded => {
             this.word = succeeded.word;
-            this.example_usage = succeeded.example_usage;
-            this.antonym = succeeded.antonym;
-            this.synonym = succeeded.synonym;
-            this.general_sense = succeeded.general_sense;
+            this.hiddenSynonym = succeeded.synonym;
+            this.hiddenAntonym = succeeded.antonym;
+            this.hiddenGeneral_sense = succeeded.general_sense;
+            this.hiddenExample_usage = succeeded.example_usage;
         })
     }
+
+    displayHint(){
+        let hintNumber = Math.floor(Math.random() * 4);
+        if (hintNumber == 0) {
+            if (this.synonym != null) {
+                this.displayHint();
+            } else {
+                this.synonym = this.hiddenSynonym;
+                this.hintCounter++;
+            }
+        }
+        else if (hintNumber == 1) {
+            if (this.antonym != null) {
+                this.displayHint();
+            } else {
+                this.antonym = this.hiddenAntonym;
+                this.hintCounter++;
+            }
+
+    }
+        else if (hintNumber == 2) {
+            if (this.general_sense != null) {
+                this.displayHint();
+            } else {
+                this.general_sense = this.hiddenGeneral_sense;
+                this.hintCounter++;
+            }
+        }
+        else if (hintNumber == 3){
+            if (this.example_usage != null) {
+                this.displayHint();
+            } else {
+                this.example_usage = this.hiddenExample_usage;
+                this.hintCounter++;
+            }
+        }
+    }
+
+
+
+    /*
+    displayHint(){
+        let hintNumber = Math.floor(Math.random() * 3);
+
+        if (hintNumber == 0) {
+           if(this.synonym != null){
+
+           }
+        }
+    }
+    */
   // constructor(word: string, synonym: string, antonym: string, general_sense: string, example_usage: string) {
   //     this.word = word;
   //     this.synonym = synonym;
