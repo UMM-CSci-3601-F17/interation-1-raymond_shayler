95b5282507cfd133920e207ab1a28a6a14cad0a8
SirLich <lrkoehler77@gmail.com>
2017-10-11 23:23:18 -0500

    Add full client-side testing for home-page

--- client/src/app/home/home.component.spec.ts
+++ client/src/app/home/home.component.spec.ts
@@ -13,6 +13,14 @@ describe('Home', () => {
     let fixture: ComponentFixture<HomeComponent>;
     let de: DebugElement;
     let el: HTMLElement;
+    let playDe: DebugElement;
+    let playEl: HTMLElement;
+    let cardDe: DebugElement;
+    let cardEl: HTMLElement;
+    let deckDe: DebugElement;
+    let deckEl: HTMLElement;
+    let settingDe: DebugElement;
+    let settingEl: HTMLElement;
 
     beforeEach(() => {
         TestBed.configureTestingModule({
@@ -30,13 +38,46 @@ describe('Home', () => {
         // query for the title <h1> by CSS element selector
         de = fixture.debugElement.query(By.css('#logo'));
         el = de.nativeElement;
+
+        playDe = fixture.debugElement.query(By.css("#play-button"));
+        playEl = playDe.nativeElement;
+
+        cardDe = fixture.debugElement.query(By.css("#card-button"));
+        cardEl = cardDe.nativeElement;
+
+        deckDe = fixture.debugElement.query(By.css("#deck-button"));
+        deckEl = deckDe.nativeElement;
+
+        settingDe = fixture.debugElement.query(By.css("#setting-button"));
+        settingEl = settingDe.nativeElement;
+
     });
 
     /*
     Test various buttons on the homePage
      */
-    it("displays a greeting", () => {
+    it("contains 'I Am Sage logo'", () => {
         fixture.detectChanges();
         expect(el.textContent).toContain("Sage");
+    });
+
+    it("contains play button", () => {
+        fixture.detectChanges();
+        expect(playEl.textContent).toContain("Play");
+    });
+
+    it("contains card button", () => {
+        fixture.detectChanges();
+        expect(cardEl.textContent).toContain("Card");
+    });
+
+    it("contains deck button", () => {
+        fixture.detectChanges();
+        expect(deckEl.textContent).toContain("Deck");
+    });
+
+    it("contains setting button", () => {
+        fixture.detectChanges();
+        expect(settingEl.textContent).toContain("Setting");
     });
 });
